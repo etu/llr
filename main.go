11018be6816a1bb081134ece8c306e8b8805d540
Elis Hirwing <elis@hirwing.se>
2022-10-13 18:28:08 +0200

    Create a scanner to read from STDIN or a file argument

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main // import "github.com/etu/llr"
 
 import (
+	"bufio"
 	"flag"
 	"golang.org/x/crypto/ssh/terminal"
 	"log"
+	"os"
 )
 
 func main() {
 	var debug bool
 	var width uint
+	var scanner *bufio.Scanner
 
 	// Detect terminal size.
-	detectedWidth, _, err := terminal.GetSize(0)
+	detectedWidth, _, err := terminal.GetSize(1)
 
 	// Error out if terminal size detection fails.
 	if err != nil {
@@ -22,4 +25,30 @@ func main() {
 	flag.BoolVar(&debug, "debug", false, "Enable or disable debug output")
 	flag.UintVar(&width, "width", uint(detectedWidth), "Specify a fixed with instead of terminal width")
 	flag.Parse()
+
+	// If we get args, try to find a filename
+	if len(flag.Args()) >= 1 {
+		filename := flag.Args()[0]
+
+		// If filename is "-", we can just consider it stdin. So just
+		// ignore this filename if we actually have a filename.
+		if filename != "-" {
+			file, err := os.Open(filename)
+
+			// Print error if it doesn't exist
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer file.Close()
+
+			scanner = bufio.NewScanner(file)
+		}
+	}
+
+	// If we don't already have a scanner from reading a file... then
+	// we fall back to reading stdin.
+	if scanner == nil {
+		scanner = bufio.NewScanner(os.Stdin)
+	}
 }
