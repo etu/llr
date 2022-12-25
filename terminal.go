282f66ece87de9c27d0b620f7001c59b31e992e7
Elis Hirwing <elis@hirwing.se>
2022-12-25 13:08:23 +0100

    Fall back to calling "stty size" to detect the terminal width

--- terminal.go
+++ terminal.go
@@ -2,19 +2,43 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
 
 // GetTerminalWidth gets the width of the terminal.
 func getTerminalWidth() int {
-	// Detect terminal size.
-	detectedWidth, _, err := terminal.GetSize(1)
+	width, _, err := terminal.GetSize(int(os.Stdout.Fd()))
 
-	// Error out if terminal size detection fails.
+	if err == nil {
+		return width
+	}
+
+	// Fall back to using 'stty size' if terminal dimensions cannot be retrieved
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, err := cmd.Output()
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return detectedWidth
+	dimensions := strings.Split(string(out), " ")
+
+	if len(dimensions) != 2 {
+		log.Fatal("Error parsing output of 'stty size'")
+	}
+
+	// Convert string to int
+	width, err = strconv.Atoi(strings.TrimRight(dimensions[1], "\n"))
+
+	if err != nil {
+		log.Fatal("Error converting string to int:", err)
+	}
+
+	return width
 }
